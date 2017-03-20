a81a98ff5fa5576ff4ac0051075cfb5d83558d04
wanglei <wanglei6@yy.com>
2017-03-20 19:47:03 +0800

    add server event frame

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,10 +1,36 @@
-#include <iostream>
+#include "vcs/event_base/event_base.h"
+#include "vcs/util/server_config.h"
+#include "vcs/doctor/server_doctor.h"
+#include "client_listener.h"
+#include <string>
 
+using namespace vcs;
 using namespace std;
 
-
 int main(int argc, char* argv[]) {
-    cout << "server code" << endl;
-    return 0;
+    if(argc != 2){
+        fprintf(stderr, "Usage:%s <config.json>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    ServerConfig* cfg   =  Singleton<ServerConfig>::instance();
+    cfg->load(argv[1]);
+
+    string doctorHost   = cfg->getStrValue("doctor.host");
+    int doctorPort      = cfg->getIntValue("doctor.port");
+    ServerDoctor::instance()->init(doctorHost.c_str(), doctorPort);
+
+    ClientListener* clientListener = Singleton<ClientListener>::instance();
+    string clientHost   = cfg->getStrValue("client.host");
+    int clientPort      = cfg->getIntValue("client.port");
+    clientListener->fd  = Helper::Socket::listen(clientHost.c_str(), (unsigned short)clientPort);
+    if(clientListener->fd < 0){
+        log_error("start fail, client listener listen on %s:%d fail", clientHost.c_str(), clientPort);
+        exit(EXIT_FAILURE);
+    }
+    EventBase::instance()->add(clientListener);
+    log_info("client listener listen on %s:%d", clientHost.c_str(), clientPort);
+
+    EventBase::instance()->dispatch();
 }
 
